b44a76f4c1a9d0a74de4f825c319f2f594c52ff8
danila-art <57034590+danila-art@users.noreply.github.com>
2020-02-12 18:55:49 +0400

    Add files via upload

--- index.php
+++ index.php
@@ -2,16 +2,35 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
     <title>Books&Authors</title>
-    <meta name="viewport" content="width=device-width, initial-scale = 1">
+    <link rel="stylesheet" href="CSS/stylesheet.css">
 </head>
 <body>
-<div>
-    <a href="INTERNAL_PAGE/ADD_BOOK.php">Страница добавления книги</a><hr>
-    <a href="INTERNAL_PAGE/DELETE_BOOK.php">Страница удаления книги</a>
-</div>
-<hr>
+<div class="container">
+    <header>
+        <div class="header">
+            <div class="head_menu">
 
-<a href="PHP_PDO/VIEW_BOOK.php">TEST</a>
+            </div>
+            <div class="head_image">
+                <img src="IMAGES/hero-curriculum.png" alt="">
+            </div>
+        </div>
+    </header>
+<div class="main">
+
+</div>
+    <footer>
+
+    </footer>
+</div>
+<!--<div>-->
+<!--    <a href="INTERNAL_PAGE/ADD_BOOK.php">Страница добавления книги</a><hr>-->
+<!--    <a href="INTERNAL_PAGE/DELETE_BOOK.php">Страница удаления книги</a>-->
+<!--</div>-->
+<!--<hr>-->
+<!---->
+<!--<a href="PHP_PDO/VIEW_BOOK.php">TEST</a>-->
 </body>
 </html>
